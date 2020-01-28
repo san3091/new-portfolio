c12a0c63cc2f2fe60c60c1d0b5511afa7a9a9ca1
Santiago Quintana <san3091@gmail.com>
2020-01-28 14:26:25 -0600

    Init

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { Router, Route } from 'svelte-routing'
+  import Nav from './Nav.svelte'
+  import Bio from './Bio.svelte'
+
+
+  export let url = ""
+</script>
+
+<main>
+  <Router url="{url}">
+    <Nav />
+    <section>
+      <Route path="bio">
+        <p>Thing!</p>
+      </Route>
+    <section>
+  </Router>
+</main>
+
+<style>
+
+  main {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 100%;
+  }
+
+  section {
+    flex: 3;
+  }
+
+  h1 {
+    font-size: 30px;
+  }
+
+  p {
+    font-size: 18px;
+  }
+
+</style>
---
+++ src/Nav.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { Router, link } from 'svelte-routing'
+  import { onMount } from 'svelte'
+  import { fly } from 'svelte/transition'
+
+  onMount(() => {
+    visible = true
+  })
+
+  let visible = false
+</script>
+
+<nav>
+  {#if visible}
+    <a href="/" use:link transition:fly="{{x: -200, duration: 800}}"><h1>Santiago<br>Quintana</h1></a>
+    <a href="/bio" use:link transition:fly="{{x: -200, duration: 1000}}">Bio</a>
+    <a href="/art" use:link transition:fly="{{x: -200, duration: 1200}}">Art</a>
+    <a href="/tech" use:link transition:fly="{{x: -200, duration: 1400}}">Tech</a>
+    <a href="/contact" use:link transition:fly="{{x: -200, duration: 1600}}">Contact</a>
+  {/if}
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 70px;
+    text-align: right;
+  }
+
+  a {
+    margin-bottom: 15px;
+  }
+
+  .active {
+    margin-right: -5px;
+    transition: margin-right 1s;
+  }
+</style>
