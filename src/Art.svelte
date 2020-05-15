244d00438dddfc5c5b1ed4518a641127d2297be9
Santiago Quintana <san3091@gmail.com>
2020-05-14 19:35:26 -0500

    added assets

--- src/Art.svelte
+++ src/Art.svelte
@@ -10,7 +10,7 @@
       description: `Presented at ADF. I created a model using a <a href="https://github.com/ibab/tensorflow-wavenet/" alt="Tensorflow Wavenet Github Repo" class="link">TensorFlow implementation of DeepMind's WaveNet paper</a>. I recorded myself reading passages from Virginia Wolf's <span class="underline">To The Lighthouse</span> and trained a model using a GPU EC2 instance on AWS. I trained the model for 99,999 iterations over the course of a week. I generated audio samples at different stages and used these audio samples to generate the final piece of music for the piece. The piece is a quartet, where each of the dancers/neurons takes on movement that I generated and interprets it at different stages, mapped onto a diagonal. The piece traces the non-linear nature of a machine learning model's training. Some checkpoints produce clearer vocalizations than others, seemingly "regressing" in the process, but those diversions are crucial pieces of the learning process.`,
       link: {
         video: "",
-        thumbnail: "",
+        thumbnail: "images/99-score.jpg",
       }
     },
     {
@@ -30,7 +30,7 @@
       description: "Inspired by the creative process of Noa Zuk and Ohad Fishof, I generated an electronic rhythmic sequence with ML Magenta in Ableton and produced movement to that structure. I wanted to explore desire and trepidation, the moment of confidently shooting your shit with your crush and the following insecurity and trepidation. The process of seduction as a codified structured and yet endlessly variating performance.",
       link: {
         video: "",
-        thumbnail: "",
+        thumbnail: "images/or-what-1.jpg",
       }
     },
     {
@@ -50,7 +50,7 @@
       description: "A broader interpretation of the structures and processes that fascinate me. Devised in collaboration with Sam, sestina uses the European poetic form to generate a structure for both music and movement. The sestina works by using six end-words and rearranging them in a specific pattern (algorithm?) to generate the order for that next stanza. Sam and I generated different 'moods' as each 'end-word' and switched between them according to the sestina's order. This piece brought up topics around the disconnection and disjointedness that can come from the rigid structures that shape our life.",
       link: {
         video: "",
-        thumbnail: ""
+        thumbnail: "images/sestina-score.jpg"
       }
     },
     {
