07e84dfb8e67a4bd0a7b75f94f2e47b411e3c6f4
Santiago Quintana <san3091@gmail.com>
2020-06-29 15:52:30 -0500

    update nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,13 +17,20 @@
 
 <nav>
   {#if visible}
-    {#each navItems as navItem, index}
-      <div transition:fly="{{x: -200, duration: calcDuration(index)}}">
-        <NavLink to={navItem.route} header="{navItem.isHeader}">
-          {@html navItem.title}
-        </NavLink>
-      </div>
-    {/each}
+    <div class="header" transition:fly="{{x: -200, duration: calcDuration(0)}}">
+      <NavLink to="/" header>
+        <h1>Santiago<br>Quintana</h1>
+      </NavLink>
+    </div>
+    <div class="items">
+      {#each navItems as navItem, index}
+        <div transition:fly="{{x: -200, duration: calcDuration(index + 1)}}">
+          <NavLink to={navItem.route} header="{navItem.isHeader}">
+            {@html navItem.title}
+          </NavLink>
+        </div>
+      {/each}
+    </div>
   {/if}
 </nav>
 
@@ -36,9 +43,30 @@
     text-align: right;
   }
 
+  .items {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
   div {
     margin-bottom: 15px;
     cursor: pointer;
+  }
+
+  @media only screen and (max-width: 1000px) {
+    nav {
+      align-items: center;
+    }
+
+    .items {
+      flex-direction: row;
+    }
+
+    div {
+      margin-bottom: 0;
+      margin-right: 10px;
+    }
   }
 
 </style>
